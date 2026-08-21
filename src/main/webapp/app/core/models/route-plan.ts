0ac9604c6012ff9c9dfec95411700fb7e1139a73
siddzoha <siddzohnexta@sheridancollege.ca>
2026-08-21 03:14:19 -0500

    Created flight-dispatch.ts service and route-plan.ts

--- src/main/webapp/app/core/models/route-plan.ts
+++ src/main/webapp/app/core/models/route-plan.ts
@@ -1,1 +1,13 @@
-export interface RoutePlan {}
+import { IAircraft } from '../../entities/aircraft/aircraft.model';
+import { IAirport } from '../../entities/airport/airport.model';
+
+export interface RoutePlan {
+  departureAirport: IAirport;
+  arrivalAirport: IAirport;
+  aircraft: IAircraft;
+  distanceNm: number;
+  initHeading: number;
+  estFlightTimeHours: number;
+  estFuelGallons: number;
+  xpReward: number;
+}
--- src/main/webapp/app/core/services/flight-dispatch.ts
+++ src/main/webapp/app/core/services/flight-dispatch.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RoutePlan } from '../models/route-plan';
 
 @Injectable({
   providedIn: 'root',
 })
-export class FlightDispatch {}
+export class FlightDispatch {
+  private http = inject(HttpClient);
+  private resourceUrl = '/api/navigation/calculate-route';
+
+  public calculateRoute(depAirportId: number, arrAirportId: number, aircraftId: number): Observable<RoutePlan> {
+    const params = new HttpParams()
+      .set('departureAirportId', depAirportId)
+      .set('arrivalAirportId', arrAirportId)
+      .set('aircraftId', aircraftId);
+
+    return this.http.get<RoutePlan>(this.resourceUrl, { params });
+  }
+}
